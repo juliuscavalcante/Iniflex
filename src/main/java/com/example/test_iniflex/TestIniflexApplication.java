77433cd81b61df40776a54d61bd8868886446e4f
Julius Cavalcante <julius.cavalcante@neobiz.com.br>
2024-06-17 19:06:56 -0300

    first commit

---
+++ src/main/java/com/example/test_iniflex/TestIniflexApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_iniflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestIniflexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestIniflexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test_iniflex/TestIniflexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_iniflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestIniflexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
